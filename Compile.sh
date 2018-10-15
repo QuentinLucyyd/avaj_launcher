453d3712f68b15e05f6a92cf718bdea301edafda
Quentin <quintonacid@gmail.com>
2018-10-15 11:36:17 +0200

    Removed Source Code

--- Compile.sh
+++
@@ -1,3 +0,0 @@
-find . -name "*.java" > sources.txt
-javac -sourcepath . @sources.txt -d .
-java simulator.Simulator _scenarios/_scenario_1.txt
--- src/simulator/transport/Flyable.java
+++
@@ -1,5 +0,0 @@
-package simulator.transport;
-
-public interface Flyable {
-    void updateConditions();
-     }
